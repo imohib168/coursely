56091088927a9c63ef3a1dd64bd1a05bbbf3c883
imohib168 <imohib168@gmail.com>
2022-07-07 16:20:05 +0500

    small fixes in blog post

--- client/src/sections/Blogs/Blog.jsx
+++ client/src/sections/Blogs/Blog.jsx
@@ -1,35 +1,24 @@
 import React from 'react';
-import { Box, Typography } from '@mui/material';
 import { StyledMainBox } from '../../styles';
 import { UIBlogCard } from '../../components';
 
 const BlogPost = () => {
-  const category = null;
+  const text = `
+        Lorem ipsum  is simply dummy text of the printing and typesetting industry 
+        is simply dummy text of the printing and typesetting industry simply dummy text of the printing and typesetting industry
+        simply dummy text of the printing and typesetting industry simply dummy text of the printing and typesetting industry
+        simply dummy text of the printing and typesetting industry simply dummy text of the printing and typesetting industry
+        simply dummy text of the printing and typesetting industry simply dummy text of the printing and typesetting industry
+  `;
 
   return (
     <StyledMainBox>
-      {category && (
-        <Typography
-          variant='h5'
-          sx={{ marginBottom: '20px', textAlign: 'right !important' }}
-        >
-          Category:{' '}
-          <Box component='span' sx={{ fontSize: '18px', fontWeight: 600 }}>
-            {category}
-          </Box>
-        </Typography>
-      )}
-
       <UIBlogCard
         isDetailPage
         title='Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum'
         username='Mohib Ismail'
         time='3m ago'
-        blogText='Lorem ipsum  is simply dummy text of the printing and typesetting industry 
-        is simply dummy text of the printing and typesetting industry simply dummy text of the printing and typesetting industry
-        simply dummy text of the printing and typesetting industry simply dummy text of the printing and typesetting industry
-        simply dummy text of the printing and typesetting industry simply dummy text of the printing and typesetting industry
-        simply dummy text of the printing and typesetting industry simply dummy text of the printing and typesetting industry'
+        blogText={text}
       />
     </StyledMainBox>
   );
--- client/src/sections/Blogs/BlogPost.jsx
+++ client/src/sections/Blogs/BlogPost.jsx
@@ -1,26 +1,11 @@
 import React from 'react';
-import { Box, Typography } from '@mui/material';
 import { StyledMainBox } from '../../styles';
 import { UIBlogCard } from '../../components';
 
 const BlogPost = () => {
-  const category = null;
-
   return (
     <StyledMainBox>
-      {category && (
-        <Typography
-          variant='h5'
-          sx={{ marginBottom: '20px', textAlign: 'right !important' }}
-        >
-          Category:{' '}
-          <Box component='span' sx={{ fontSize: '18px', fontWeight: 600 }}>
-            {category}
-          </Box>
-        </Typography>
-      )}
-
-      {[1, 1, 1, 1, 1, 1, 1].map((_, index) => (
+      {[1, 1].map((_, index) => (
         <UIBlogCard
           key={index}
           title='Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum'
