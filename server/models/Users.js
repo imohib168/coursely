2f27ebe3eb8ac78c6df71a0686132faf93800aca
imohib168 <imohib168@gmail.com>
2022-07-08 20:04:31 +0500

    fixed response data & error issue for user

--- server/models/Users.js
+++ server/models/Users.js
@@ -21,7 +21,7 @@ module.exports = (sequelize, DataTypes) => {
     roleId: { type: DataTypes.INTEGER },
     firstName: { type: DataTypes.STRING },
     lastName: { type: DataTypes.STRING },
-    phoneNumber: { type: DataTypes.STRING },
+    phone: { type: DataTypes.STRING },
     githubURL: { type: DataTypes.STRING },
     linkedinURL: { type: DataTypes.STRING },
     facebookURL: { type: DataTypes.STRING },
